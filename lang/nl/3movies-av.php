3d0bd12738ea108122abf0ef870b7fc89daeed71
davidvanbeveren <david.vanbeveren@gmail.com>
2022-01-16 17:00:12 +0100

    Dutch translation

--- lang/nl/3movies-av.php
+++ lang/nl/3movies-av.php
@@ -4,34 +4,34 @@ return [
     /* av.3movies.org translation files */
 
     // top-level elements
-    'title' => 'Middelen - Anoniem voor de stemloze',
-    'header' => 'Dieren vermoord in het vlees, zuivel- en eierindustrie, aangezien u deze pagina hebt geopend:',
+    'title' => 'Hulpmiddelen - Anonymous for the Voiceless',
+    'header' => 'Het aantal dieren dat vermoord is in de vlees, zuivel- en eierindustrie, sinds u deze pagina hebt geopend:',
 
     // top navbar
     'nav.why' => 'WAAROM VEGAN?',
-    'nav.cube' => 'Kubus van waarheid',
+    'nav.cube' => 'Cube of Truth',
     'nav.donate' => 'Steun ons',
     'nav.shop' => 'Winkel',
     'nav.who' => 'Wie we zijn',
     'nav.join' => 'Doe met ons mee',
-    'nav.join.title' => 'Aansluiten bij',
+    'nav.join.title' => 'Aansluiten bij AV',
 
     // where-did-you-find-us modal
-    'modal.welcome' => 'Toegang tot onze bronnen',
+    'modal.welcome' => 'Krijg toegang tot onze hulpmiddelen',
     'modal.referrer' => 'Waar heb je ons gevonden?',
     'modal.placeholder' => 'Voer de naam van de stad in',
     'modal.submit' => 'Indienen',
 
     // 'Are you ready now to take action?' panel at the bottom
     'action.title' => 'Ben je nu klaar om actie te ondernemen?',
-    'action.become' => 'Word een activist van dierenrechten',
-    'action.av' => 'Anoniem voor de voiceless (AV) is een niet-winstige dierenrechtenorganisatie die gespecialiseerd is in het gebruik van een gesprek en de standaardpraktijk met de praktijk om het publiek over exploitatie van dieren te beperken.In eerste instantie opgericht in Melbourne, Australië, zijn we nu een wereldwijde gemeenschap van gelijkgestemde mensen die onze stemmen voor gerechtigheid gebruiken voor onze mede-aardappel.Om de grootste impact te maken, moeten we mobiliseren en samenwerken.',
-    'action.join' => 'Word lid van AV in 2 eenvoudige stappen',
-    'action.join.title' => 'Aansluiten bij',
+    'action.become' => 'Sluit je aan bij de meest productieve dierenrechtenorganisatie ter wereld',
+    'action.av' => 'Anonymous for the Voiceless (AV) is een non-profit dierenrechtenorganisatie die gespecialiseerd is in straatactivisme, waarbij we gesprekken voeren en standaardpraktijkbeelden tonen om voorbijgangers voor the lichten over de uitbuiting van dieren. We zijn oorspronkelijk opgericht in Melbourne, Australië, en vormen nu een wereldwijde gemeenschap van gelijkgestemde mensen die opkomen voor de rechten van andere dieren. Om de grootste impact te maken, moeten we mobiliseren en samenwerken.',
+    'action.join' => 'Word lid van AV in 2 stappen',
+    'action.join.title' => 'Aansluiten bij AV',
 
     // resources
-    'resources.justice' => 'Dierrechten zijn een kwestie van gerechtigheid voor niet-menselijke dieren',
-    'resources.essential' => 'De essentiële videoverlijst',
+    'resources.justice' => 'Dierenrechten zijn een kwestie van gerechtigheid. Word veganist en maak een eind aan de onderdrukking van dieren',
+    'resources.essential' => 'De essentiële videolijst',
     'resources.more-info' => 'Op zoek naar meer info?',
 
     'resources.website' => 'Bezoek website',
@@ -40,18 +40,18 @@ return [
     'resources.video' => 'Bekijk video',
 
     // footer
-    'footer.title' => 'Word lid van de frontlinie',
-    'footer.desc' => 'Wij zijn een wereldwijde organisatie van straatactivisten die zich toeleggen op het geven van een stem aan het stemloze.Doe mee en begin een verschil te maken.',
-    'footer.join' => 'Doe met ons mee',
-    'footer.join.title' => 'Aansluiten bij',
-    'footer.resources' => 'Bronnen',
-    'footer.resources.find' => 'Word lid van je lokale hoofdstuk',
-    'footer.resources.start' => 'Start uw eigen hoofdstuk',
+    'footer.title' => 'Sluit je aan',
+    'footer.desc' => 'Wij zijn een wereldwijde organisatie van toegwijde dierenrechtenactivisten. Doe mee en spreek je uit tegen de onderdrukking van andere dieren.',
+    'footer.join' => 'Sluit je aan',
+    'footer.join.title' => 'Aansluiten bij AV',
+    'footer.resources' => 'Recourses',
+    'footer.resources.find' => 'Word lid van je lokale chapter',
+    'footer.resources.start' => 'Start uw eigen chapter',
     'footer.resources.donate' => 'Doneren',
-    'footer.resources.contact' => 'Neem contact op',
+    'footer.resources.contact' => 'Contact',
     'footer.shop' => 'Winkel',
     'footer.shop.shop' => 'Haal je uitrusting',
     'footer.shop.account' => 'Mijn rekening',
-    'footer.shop.gift-cards' => 'FAQ voor geschenkkaart',
+    'footer.shop.gift-cards' => 'Gift Card FAQ',
     'footer.shop.help' => 'Winkel Helpcentrum',
 ];
