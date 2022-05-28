6775b006e62e3d7b1c1f1053f31dd08481d5dd1b
Jérémy Touati <sielver@gmail.com>
2022-05-28 16:02:33 +0200

    Fix some japanese translations

--- lang/ja/3movies-av.php
+++ lang/ja/3movies-av.php
@@ -5,15 +5,15 @@ return [
 
     // top-level elements
     'title' => 'リソース - 無声の匿名',
-    'header' => 'あなたがこのページを開けてから、肉、乳製品＆卵産業で殺害された動物',
+    'header' => 'あなたがこのページを開いてから、食肉・酪農・鶏卵業界でこれだけの動物が殺されました：',
 
     // top navbar
-    'nav.why' => 'なぜビーガン？',
-    'nav.cube' => '真実の立方体',
+    'nav.why' => 'なぜVEGAN?',
+    'nav.cube' => 'Cube of Truth',
     'nav.donate' => '私たちを応援してください',
-    'nav.shop' => '店',
-    'nav.who' => '誰がいます',
-    'nav.join' => '参加しませんか',
+    'nav.shop' => 'Shop',
+    'nav.who' => '私達について',
+    'nav.join' => '参加しよう',
     'nav.join.title' => 'に参加する',
 
     // where-did-you-find-us modal
@@ -23,15 +23,15 @@ return [
     'modal.submit' => '送信',
 
     // 'Are you ready now to take action?' panel at the bottom
-    'action.title' => 'あなたは今行動を起こす準備ができていますか？',
-    'action.become' => '動物の権利活動家になる',
-    'action.av' => '無声（AV）の匿名は、動物の搾取に関する一般の人々を診察するために会話と標準的な実践映像の使用を専門とする非営利の動物権利機関です。オーストラリアのメルボルンで最初に設立されている、私たちは今、私たちの仲間の地球のための正義のために私たちの声を使っているような志向の人々の世界的なコミュニティです。最大の影響を与えるために、私たちは動員して一緒に働く必要があります。',
-    'action.join' => '2簡単なステップでAVに参加してください',
-    'action.join.title' => 'に参加する',
+    'action.title' => '行動を起こしたいですか？',
+    'action.become' => '動物の権利の活動家になろう',
+    'action.av' => '無声アノニマス・フォー・ザ・ボイスレス（AV）は、会話と一般的な現場映像を使って動物搾取について一般の人々に啓発することに特化した非営利アニマルライツ団体です。当初はオーストラリアのメルボルンで設立され、現在は地球の生き物達の正義のために一緒に声を上げている人々の世界的なコミュニティです。最大の効果を発揮するために、みんなが一緒に協力して動く必要があります。',
+    'action.join' => '簡単な2ステップでAVに参加しよう',
+    'action.join.title' => '最前線に参加しよう',
 
     // resources
-    'resources.justice' => '動物の権利は、人間の非動物のための正義の問題です',
-    'resources.essential' => '必須ビデオリスト',
+    'resources.justice' => '動物の権利（アニマルライツ）は人間以外の動物たちの正義の問題です',
+    'resources.essential' => '必見動画リスト',
     'resources.more-info' => 'もっと情報をお探しですか？',
 
     'resources.website' => 'サイトを訪れる',
@@ -41,17 +41,17 @@ return [
 
     // footer
     'footer.title' => '最前線に参加してください',
-    'footer.desc' => '私たちは無声に声を与えることに捧げられたストリート活動家の世界的な組織です。私たちに参加して違いを始めます。',
+    'footer.desc' => '私たちは声無き者に声を与えることに尽くす路上活動家の世界的な組織です。一緒に参加して変化を起こそう。',
     'footer.join' => '参加しませんか',
     'footer.join.title' => 'に参加する',
-    'footer.resources' => '資力',
-    'footer.resources.find' => 'あなたの地元の章に参加してください',
-    'footer.resources.start' => 'あなた自身の章を始めます',
+    'footer.resources' => '参考',
+    'footer.resources.find' => '近くのチャプターに参加',
+    'footer.resources.start' => '自分のチャプターを始める',
     'footer.resources.donate' => '寄付',
     'footer.resources.contact' => 'お問い合わせ',
-    'footer.shop' => '店',
-    'footer.shop.shop' => 'あなたのギアを手に入れよう',
-    'footer.shop.account' => 'マイアカウント',
+    'footer.shop' => 'Shop',
+    'footer.shop.shop' => 'ギアを買おう',
+    'footer.shop.account' => 'My アカウント',
     'footer.shop.gift-cards' => 'ギフトカードFAQ',
-    'footer.shop.help' => 'ショップヘルプセンター',
+    'footer.shop.help' => 'Shopについてのヘルプ',
 ];
