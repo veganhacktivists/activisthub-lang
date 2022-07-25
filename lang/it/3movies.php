cd66fd73ee36f9c25065d61ac93dd96510257d86
Jérémy Touati <sielver@gmail.com>
2022-07-25 22:36:16 +0200

    Add italian translation for 3Movies (#9)

---
+++ lang/it/3movies.php
@@ -0,0 +1,83 @@
+<?php
+
+return [
+    /* 3movies.org translation files */
+
+    // top-level elements
+    'title' => '3 movies that will change your life | 3movies.org',
+    'header' => "We're here to help you on your vegan journey!",
+    'subheader' => 'Watch three thought-provoking movies that shed light on uncomfortable realities, and explore other resources for support!',
+
+    // enter-your-code modal
+    'modal.welcome' => 'Welcome to 3Movies.org',
+    'modal.description' => 'The resources on this site expose facts that are intentionally hidden from consumers.',
+    'modal.enter-code' => 'Please enter your code:',
+    'modal.no-code' => "Don't have a code?",
+    'modal.yes-code' => 'Have a code?',
+    'modal.referrer' => 'Where did you hear about us?',
+    'modal.referrer.event' => 'Street Event',
+    'modal.referrer.social' => 'Social Media',
+    'modal.referrer.rec' => 'Friends / Family',
+    'modal.powered-by' => 'Powered by <a href=":url">ActivistHub.org</a>',
+
+    // free-vegan-mentor element
+    'mentor.signup' => 'Sign up for your',
+    'mentor.header' => 'Free Vegan Mentor!',
+    'mentor.desc.1' => "We've partnered with Vegan Outreach's Vegan Mentor Program to help ease the transition by providing one-on-one personal support!",
+    'mentor.desc.2' => "You'll be paired with your own mentor who can answer questions and guide you with tips on shopping, cooking, eating out, nutrition information, and more. The program strives to foster a healthy, sustainable lifestyle shift by offering a network of support and resources that promote kindness over cruelty.",
+    'mentor.request-button' => 'Request a Mentor',
+
+    // footer
+    'footer.hub-link' => 'Customize and share this page via <a href=":url" target="_blank"><u>Activist Hub!</u></a>',
+    'footer.vh-link' => 'A project by the <a href=":url" target="_blank"><u>Vegan Hacktivists</u></a>',
+    'footer.patreon-link' => 'Support Us',
+
+    // resources
+    'resources.website' => 'Visit Website',
+    'resources.youtube' => 'Watch on YouTube',
+    'resources.netflix' => 'Watch on Netflix',
+    'resources.video' => 'Watch Video',
+    'resources.video-too-slow-external-link' => 'Video loading too slow?',
+    'resources.video-too-slow-external-link-action' => 'Watch on :domain!',
+
+    // resource categories. This should match available categories in \App\Models\Resources::MAIN_CATEGORY_TAGS
+    'resources.category.movie' => 'Movies',
+    'resources.category.video' => 'Videos',
+    'resources.category.challenge' => 'Challenges',
+    'resources.category.book' => 'Books',
+    'resources.category.website' => 'Websites',
+    'resources.category.podcast' => 'Podcasts',
+    'resources.category.recipe' => 'Recipes',
+    'resources.category.blog' => 'Blogs',
+    'resources.category.audiobook' => 'Audiobooks',
+
+    'resources.title.dominion' => 'Dominion',
+    'resources.desc.dominion' => "Dominion utilizza droni, telecamere nascoste e portatili per esporre il ventre oscuro del moderno allevamento animale, mettendo in discussione la moralità e la validità del dominio dell'uomo sul regno animale.",
+    'resources.title.seaspiracy' => 'Seaspiracy',
+    'resources.desc.seaspiracy' => 'Passionate about ocean life, a filmmaker sets out to document the harm that humans do to marine species — and uncovers alarming global corruption.',
+    'resources.title.the-game-changers' => 'The Game Changers',
+    'resources.desc.the-game-changers' => 'Meeting visionary scientists and top athletes, a UFC fighter embarks on a quest to find the optimal diet for human performance and health.',
+    'resources.title.cowspiracy' => 'Cowspiracy',
+    'resources.desc.cowspiracy' => 'Follow the shocking, yet humorous, journey of an aspiring environmentalist, as he daringly seeks to find the real solution to the most pressing environmental issues and true path to sustainability.',
+    'resources.title.forks-over-knives' => '',
+    'resources.desc.forks-over-knives' => '',
+    'resources.title.best-of-gary-yourofsky' => 'Il meglio di Gary Yourofsky',
+    'resources.desc.best-of-gary-yourofsky' => "Milioni di persone sono diventate vegane dopo aver ascoltato l'attivista per i diritti degli animali, Gary Yourofsky.",
+    'resources.title.norm-a-short-film' => '"Norm" - un cortometraggio',
+    'resources.desc.norm-a-short-film' => 'Due coinquilini che discutono sulla moralità dello stupro, in una società dove stuprare le donne è legale e socialmente accettato.',
+    'resources.title.happy-cow-find-vegan-restaurants-nearby' => 'Happy Cow - Trova i ristoranti vegani nelle vicinanze',
+    'resources.desc.happy-cow-find-vegan-restaurants-nearby' => 'Happy Cow troverà i ristoranti e i negozi più vicini a te che vendono ottimo cibo vegano, non importa dove vivi!',
+    'resources.title.adapttorg' => 'Adaptt.org – in inglese',
+    'resources.desc.adapttorg' => 'Fondata nel 1996, ADAPTT risponderà a tutte le domande che potresti avere sui vegani, e nel frattempo sfata tutti i miti anti-vegani!',
+    'resources.title.100-plant-based-recipes-community' => 'Comunità di ricette 100% vegetali – in inglese',
+    'resources.desc.100-plant-based-recipes-community' => 'La più grande comunità di ricette vegane del mondo con nuove ricette 100% vegetali ogni giorno! Unisciti alla comunità e trova subito ricette gustose e convenienti!',
+
+    'resources.feedback.love' => 'WE LOVE FEEDBACK!',
+    'resources.feedback.submit' => 'Submit',
+    'resources.feedback.success' => 'Successfully submitted feedback!',
+    'resources.feedback.question.default.multi' => 'What did you think of this resource?',
+    'resources.feedback.question.default.multi.answer.1' => 'It had no significant impact on me.',
+    'resources.feedback.question.default.multi.answer.2' => 'It inspired me to do more research.',
+    'resources.feedback.question.default.multi.answer.3' => 'It inspired me to reduce the number of animal products I buy.',
+    'resources.feedback.question.default.multi.answer.4' => 'It inspired me to buy only vegan products.',
+];
