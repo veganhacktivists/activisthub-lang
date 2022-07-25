38c7d74eb38f9f9566bf290dc2c659c4633025ef
Jérémy Touati <sielver@gmail.com>
2022-07-25 22:36:14 +0200

    Add italian translation for 3Movies

---
+++ lang/it/3movies-av.php
@@ -0,0 +1,66 @@
+<?php
+
+return [
+    /* av.3movies.org translation files */
+
+    // top-level elements
+    'title' => 'Resources - Anonymous for the Voiceless',
+    'header' => 'Animals murdered in the meat, dairy & egg industries since you opened this page:',
+
+    // top navbar
+    'nav.why' => 'Perché diventare vegano?',
+    'nav.cube' => 'Cube of Truth',
+    'nav.donate' => 'Supportaci',
+    'nav.shop' => 'Shop',
+    'nav.who' => 'Chi siamo',
+    'nav.join' => 'Unisciti a noi',
+    'nav.join.title' => 'Unisciti ad AV',
+
+    // where-did-you-find-us modal
+    'modal.welcome' => 'Access our resources',
+    'modal.referrer' => 'Where did you find us?',
+    'modal.placeholder' => 'Enter city name',
+    'modal.submit' => 'Submit',
+
+    // 'Are you ready now to take action?' panel at the bottom
+    'action.title' => 'SEI PRONTO AD AGIRE?',
+    'action.become' => "UNISCITI ALL'ORGANIZZAZIONE PER I DIRITTI DEGLI ANIMALI PIÙ ATTIVA DEL MONDO",
+    'action.av' => "Anonymous for the Voiceless (AV) è un'organizzazione no-profit per i diritti degli animali specializzata nell'utilizzo di conversazioni e filmati standard per sensibilizzare il pubblico sullo sfruttamento degli animali. Inizialmente fondata a Melbourne, in Australia, siamo ora una comunità globale di persone che la pensano come noi e che usano le loro voci per chiedere giustizia per i nostri compagni di vita. Per ottenere il massimo impatto, dobbiamo mobilitarci e lavorare insieme.",
+    'action.join' => 'Unisciti ad AV in 2 facili passi',
+    'action.join.title' => 'Unisciti ad AV',
+
+    // resources
+    'resources.justice' => 'I DIRITTI DEGLI ANIMALI SONO UNA QUESTIONE DI GIUSTIZIA PER GLI ANIMALI NON UMANI, DIVENTA VEGANO E METTI FINE ALLA SUPREMAZIA UMANA',
+    'resources.essential' => 'I VIDEO ESSENZIALI',
+    'resources.more-info' => 'CERCHI MAGGIORI INFORMAZIONI?',
+
+    'resources.website' => 'Visit Website',
+    'resources.youtube' => 'Watch on YouTube',
+    'resources.netflix' => 'Watch on Netflix',
+    'resources.video' => 'Watch Video',
+
+    // footer
+    'footer.title' => 'Support our work',
+    'footer.desc' => 'We are a worldwide organization of street activists dedicated to ending animal abuse. Support our work and play a vital role in enabling us to continue the work we do for animals.',
+    'footer.join' => 'Support Us',
+    'footer.join.title' => 'Support Us',
+    'footer.resources' => 'Resources',
+    'footer.resources.find' => 'Join Your Local Chapter',
+    'footer.resources.start' => 'Start Your Own Chapter',
+    'footer.resources.donate' => 'Donate',
+    'footer.resources.contact' => 'Contact Us',
+    'footer.shop' => 'Shop',
+    'footer.shop.shop' => 'Get Your Gear',
+    'footer.shop.account' => 'My Account',
+    'footer.shop.gift-cards' => 'Gift Card FAQ',
+    'footer.shop.help' => 'Shop Help Center',
+
+    'resources.feedback.love' => 'WE LOVE FEEDBACK!',
+    'resources.feedback.submit' => 'Submit',
+    'resources.feedback.success' => 'Successfully submitted feedback!',
+    'resources.feedback.question.default.multi' => 'What did you think of this resource?',
+    'resources.feedback.question.default.multi.answer.1' => 'It had no significant impact on me.',
+    'resources.feedback.question.default.multi.answer.2' => 'It inspired me to do more research.',
+    'resources.feedback.question.default.multi.answer.3' => 'It inspired me to go vegan.',
+    'resources.feedback.question.default.multi.answer.4' => 'It inspired me to go vegan and become an activist.',
+];
